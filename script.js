f98417b9e631f944f599d83162487db64e45a8cb
Itzthurealmoreno <165456123+Itzthurealmoreno@users.noreply.github.com>
2026-02-02 04:22:20 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,51 +1,53 @@
 const sobre = document.getElementById('sobre-container');
 const carta = document.getElementById('carta-container');
+const botonsi = document.getElementById('botonsi');
 const botonno = document.getElementById('botonno');
-const botonsi = document.querySelector('.botones[alt="si"]');
 
 const tittle = document.getElementById('letter-tittle');
 const catimg = document.getElementById('carta-gato');
-const botones = document.getElementById("letter-botones")
-const textofinal = document.getElementById("texto-final")
+const botonesContenedor = document.getElementById("letter-botones");
+const textofinal = document.getElementById("texto-final");
 
-envelope.addEventListener('click', () => {
-    envelope.style,display = 'none';
-    letter.style.display = 'none';
+let yesscale = 1;
+
+sobre.addEventListener('click', () => {
+    sobre.style.display = 'none';
+    carta.style.display = 'flex';
 
     setTimeout(() => {
-        document.queryselector(".carta-ventana").classlist.add("open");
-    },50);
- });
+        document.querySelector(".carta-ventana").classList.add("open");
+    }, 50);
+});
 
+// Logic to move the NO btn
 
+let yesScale = 1;
 
- let yesScale = 1;
 
 botonsi.style.position = "relative"
-botonsi.style.transformorigin = "center center"
+botonsi.style.transformOrigin = "center center"
 botonsi.style.transition = "transform 0.3s ease";
 
 botonno.addEventListener('click', () => {
     yesScale += 2;
 
-    if(botonsi.style.position !== fixed){
+    if(botonsi.style.position !== "fixed"){
         botonsi.style.position = "fixed";
         botonsi.style.top = "50%";
         botonsi.style.left = "50%";
         botonsi.style.transform = `translate(-50%, -50%) scale(${yesScale})`;
-        }else{
-            botonsi.style.transform = `translate(-50%, -50%) scale(${yesScale})`;
+    }else{
+        botonsi.style.transform = `translate(-50%, -50%) scale(${yesScale})`;
         }
 });
 
 botonsi.addEventListener('click', () => {
     tittle.textContent = "¡SABIA YO JSJS!";
-
     catimg.src = 'moisegato.png';
+    
+    document.querySelector(".carta-ventana").classList.add("final");
+    botonsi.style.display = "none";
+    botonno.style.display = "none";
 
-document.querySelector(".carta-ventana").classList.add("final");
-
-buttons.style.display = "none";
-
-textofinal.style.display = "block";
+    textofinal.style.display = "block";
 });
